9841806cf2be95be91915822e63a1042baf8e1c5
lorecolo <lorenzo.colonniello@insiel.it>
2015-02-19 14:13:39 +0100

    GIT-1 #resolve #time 2d 5h #comment Task completed ahead of aa
    GIT-1 #resolve #time 2d 5h #comment Task completed ahead of aa

--- src/test/testMain.java
+++ src/test/testMain.java
@@ -3,7 +3,7 @@ package test;
 public class testMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubss zzzz ss fffiiiddzzz
+		// TODO Auto-generated method stubss zzzz ss fffiiiddzzzddddd
 
 		
 		System.out.print("aaa");
