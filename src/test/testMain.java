acde47c6c5f76b6eeb70144d507995a04b0476a1
lorecolo <lorenzo.colonniello@insiel.it>
2015-02-19 14:08:17 +0100

    GIT-1 #resolve #time 2d 5h #comment Task completed ahead of schedule
    GIT-1 #resolve #time 2d 5h #comment Task completed ahead of schedule

--- src/test/testMain.java
+++ src/test/testMain.java
@@ -3,7 +3,7 @@ package test;
 public class testMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubss zzzz ss fff
+		// TODO Auto-generated method stubss zzzz ss fffiii
 
 		
 		System.out.print("aaa");
