68d31ea1e82fc0a86eab8e412a1b7c9b231eeddd
lorecolo <lcolonniello@yahoo.it>
2015-02-19 17:23:52 +0100

    GIT-1 #close Fixed this today
    GIT-1 #close Fixed this today

--- src/test/testMain.java
+++ src/test/testMain.java
@@ -3,7 +3,7 @@ package test;
 public class testMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubss zzzz ss fffiiiddzzzdddddssswwssiiiddddsssdddddddddddssds
+		// TODO Auto-generated method stubss zzzz ss fffiiiddzzzdddddssswwssiiiddddsssdddddddddddssdsss
 
 		
 		System.out.print("aaa");
