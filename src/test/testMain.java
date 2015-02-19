42956cfd25aadde3d85e4304a8d414fb3e6b522f
lorecolo <lorenzo.colonniello@insiel.it>
2015-02-19 15:19:04 +0100

    GIT-1  #resolve
    GIT-1  #resolve

--- src/test/testMain.java
+++ src/test/testMain.java
@@ -3,7 +3,7 @@ package test;
 public class testMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubss zzzz ss fffiiiddzzzdddddssswwssiiidddd
+		// TODO Auto-generated method stubss zzzz ss fffiiiddzzzdddddssswwssiiiddddsss
 
 		
 		System.out.print("aaa");
